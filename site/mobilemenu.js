88a70a1a1612c556e74626c810d2c0989d0b73a0
MariCassano <105075446+MariCassano@users.noreply.github.com>
2023-05-24 09:35:26 -0300

    Add files via upload

---
+++ site/mobilemenu.js
@@ -0,0 +1,11 @@
+class Mobilemenu{
+    constructor(Mobilemenu, navList, navLinks){
+        this.Mobilemenu= document.querySelector(Mobilemenu);
+        this.navList=document.querySelector(navList);
+        this.navLinks=document.querySelectorAll(navLinks);
+        this.activeClass= "active";
+    }
+    addClickEvent(){
+        this.Mobilemenu.addEventListener
+    }
+}
